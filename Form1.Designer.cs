cd35c38cb2454986b2fb491cf24c0f57bb17db28
Virm31 <max.zaxqazaq@gmail.com>
2023-03-01 18:13:42 +0500

    Форма

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,5 +1,4 @@
-﻿
-namespace Kinoprokat
+﻿namespace WindowsFormsApp1
 {
     partial class Form1
     {
@@ -29,20 +28,180 @@ namespace Kinoprokat
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 39);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(587, 731);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Первый отчет";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(134, 10);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Второй отчет";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(243, 10);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(96, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Третий отчет";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(476, 10);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(122, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Обновить данные";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.AllowUserToResizeColumns = false;
+            this.dataGridView2.AllowUserToResizeRows = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(622, 72);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.RowHeadersVisible = false;
+            this.dataGridView2.Size = new System.Drawing.Size(467, 279);
+            this.dataGridView2.TabIndex = 5;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.AllowUserToAddRows = false;
+            this.dataGridView3.AllowUserToDeleteRows = false;
+            this.dataGridView3.AllowUserToResizeColumns = false;
+            this.dataGridView3.AllowUserToResizeRows = false;
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(624, 453);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowHeadersVisible = false;
+            this.dataGridView3.Size = new System.Drawing.Size(465, 279);
+            this.dataGridView3.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(621, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Актульное расписание сеансов:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(628, 428);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Актуальный каталог фильмов:";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(798, 738);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(128, 23);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Внести изменения";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(798, 357);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(128, 23);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Внести изменения";
+            this.button6.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(859, 478);
+            this.ClientSize = new System.Drawing.Size(1101, 782);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -8,13 +8,333 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Kinoprokat
+namespace WindowsFormsApp1
 {
+    
     public partial class Form1 : Form
     {
+        DataTable dt1;
+        DataTable dt2;
+        DataTable dt3;
+        DataTable dt4;
+        string[,] data1;
+        string[,] data2;
+        string[,] data3;
+        string[,] data4;
         public Form1()
         {
             InitializeComponent();
+
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            string[] columnNames1 = { "Фильм", "Дата релиза", "Неделя проката", "Зрители", 
+                "Сборы", "Сеансы", "Средняя стоимость билетов" };
+            dt1 = CreateDataTable(columnNames1);
+
+            string[,] data1 = {
+                { "Фильм 1", new DateTime(2023, 3, 1).ToString(), "1", "100", "1500", "10", "150" },
+                { "Фильм 2", new DateTime(2023, 3, 8).ToString(), "2", "200", "3000", "20", "180" },
+                { "Фильм 3", new DateTime(2023, 3, 15).ToString(), "3", "150", "2000", "15", "170" },
+                { "Фильм 1", new DateTime(2023, 3, 1).ToString(), "1", "100", "1500", "10", "150" },
+                { "Фильм 2", new DateTime(2023, 3, 8).ToString(), "2", "200", "3000", "20", "180" },
+                { "Фильм 3", new DateTime(2023, 3, 15).ToString(), "3", "150", "2000", "15", "170" },
+                { "Фильм 1", new DateTime(2023, 3, 1).ToString(), "1", "100", "1500", "10", "150" },
+                { "Фильм 2", new DateTime(2023, 3, 8).ToString(), "2", "200", "3000", "20", "180" },
+                { "Фильм 3", new DateTime(2023, 3, 15).ToString(), "3", "150", "2000", "15", "170" },
+                { "Фильм 1", new DateTime(2023, 3, 1).ToString(), "1", "100", "1500", "10", "150" },
+                { "Фильм 2", new DateTime(2023, 3, 8).ToString(), "2", "200", "3000", "20", "180" },
+                { "Фильм 3", new DateTime(2023, 3, 15).ToString(), "3", "150", "2000", "15", "170" },
+                { "Фильм 1", new DateTime(2023, 3, 1).ToString(), "1", "100", "1500", "10", "150" },
+                { "Фильм 2", new DateTime(2023, 3, 8).ToString(), "2", "200", "3000", "20", "180" },
+                { "Фильм 3", new DateTime(2023, 3, 15).ToString(), "3", "150", "2000", "15", "170" },
+                { "Фильм 1", new DateTime(2023, 3, 1).ToString(), "1", "100", "1500", "10", "150" },
+                { "Фильм 2", new DateTime(2023, 3, 8).ToString(), "2", "200", "3000", "20", "180" },
+                { "Фильм 3", new DateTime(2023, 3, 15).ToString(), "3", "150", "2000", "15", "170" },
+                { "Фильм 1", new DateTime(2023, 3, 1).ToString(), "1", "100", "1500", "10", "150" },
+                { "Фильм 2", new DateTime(2023, 3, 8).ToString(), "2", "200", "3000", "20", "180" },
+                { "Фильм 3", new DateTime(2023, 3, 15).ToString(), "3", "150", "2000", "15", "170" },
+                { "Фильм 1", new DateTime(2023, 3, 1).ToString(), "1", "100", "1500", "10", "150" },
+                { "Фильм 2", new DateTime(2023, 3, 8).ToString(), "2", "200", "3000", "20", "180" },
+                { "Фильм 3", new DateTime(2023, 3, 15).ToString(), "3", "150", "2000", "15", "170" },
+                { "Фильм 1", new DateTime(2023, 3, 1).ToString(), "1", "100", "1500", "10", "150" },
+                { "Фильм 2", new DateTime(2023, 3, 8).ToString(), "2", "200", "3000", "20", "180" },
+                { "Фильм 3", new DateTime(2023, 3, 15).ToString(), "3", "150", "2000", "15", "170" },
+                { "Фильм 1", new DateTime(2023, 3, 1).ToString(), "1", "100", "1500", "10", "150" },
+                { "Фильм 2", new DateTime(2023, 3, 8).ToString(), "2", "200", "3000", "20", "180" },
+                { "Фильм 3", new DateTime(2023, 3, 15).ToString(), "3", "150", "2000", "15", "170" },
+                { "Фильм 1", new DateTime(2023, 3, 1).ToString(), "1", "100", "1500", "10", "150" },
+                { "Фильм 2", new DateTime(2023, 3, 8).ToString(), "2", "200", "3000", "20", "180" },
+                { "Фильм 3", new DateTime(2023, 3, 15).ToString(), "3", "150", "2000", "15", "170" },
+                { "Фильм 1", new DateTime(2023, 3, 1).ToString(), "1", "100", "1500", "10", "150" },
+                { "Фильм 2", new DateTime(2023, 3, 8).ToString(), "2", "200", "3000", "20", "180" },
+                { "Фильм 3", new DateTime(2023, 3, 15).ToString(), "3", "150", "2000", "15", "170" },
+                { "Фильм 1", new DateTime(2023, 3, 1).ToString(), "1", "100", "1500", "10", "150" },
+                { "Фильм 2", new DateTime(2023, 3, 8).ToString(), "2", "200", "3000", "20", "180" },
+                { "Фильм 3", new DateTime(2023, 3, 15).ToString(), "3", "150", "2000", "15", "170" },
+                { "Фильм 1", new DateTime(2023, 3, 1).ToString(), "1", "100", "1500", "10", "150" },
+                { "Фильм 2", new DateTime(2023, 3, 8).ToString(), "2", "200", "3000", "20", "180" },
+                { "Фильм 3", new DateTime(2023, 3, 15).ToString(), "3", "150", "2000", "15", "170" },
+                { "Фильм 1", new DateTime(2023, 3, 1).ToString(), "1", "100", "1500", "10", "150" },
+                { "Фильм 2", new DateTime(2023, 3, 8).ToString(), "2", "200", "3000", "20", "180" },
+                { "Фильм 3", new DateTime(2023, 3, 15).ToString(), "3", "150", "2000", "15", "170" },
+            };
+
+            AddDataToDataTable(dt1, data1);
+            dataGridView1.DataSource = dt1;
+
+            // создаем вторую таблицу
+            string[] columnNames2 = { "Фильм", "Количество сеансов", "Доля сеансов", 
+                "Доля кассовых сборов" };
+            dt2 = CreateDataTable(columnNames2);
+
+            string[,] data2 = {
+                { "Фильм 1", "10", "20%", "30%" },
+                { "Фильм 2", "20", "40%", "50%" },
+                { "Фильм 3", "15", "30%", "20%" },
+                { "Фильм 1", "10", "20%", "30%" },
+                { "Фильм 2", "20", "40%", "50%" },
+                { "Фильм 3", "15", "30%", "20%" },
+                { "Фильм 1", "10", "20%", "30%" },
+                { "Фильм 2", "20", "40%", "50%" },
+                { "Фильм 3", "15", "30%", "20%" },
+                { "Фильм 1", "10", "20%", "30%" },
+                { "Фильм 2", "20", "40%", "50%" },
+                { "Фильм 1", "10", "20%", "30%" },
+                { "Фильм 2", "20", "40%", "50%" },
+                { "Фильм 3", "15", "30%", "20%" },
+                { "Фильм 3", "15", "30%", "20%" },
+            };
+
+            AddDataToDataTable(dt2, data2);
+
+            // создаем третью таблицу
+            string[] columnNames3 = { "Фильм", "Активная покупка", "Неактивная покупка", 
+                "Средняя заполняемость зала", "Средняя цена билета" };
+            dt3 = CreateDataTable(columnNames3);
+
+            string[,] data3 = {
+                { "Фильм 1", "50%", "50%", "70%", "120" },
+                { "Фильм 2", "40%", "60%", "80%", "150" },
+                { "Фильм 3", "30%", "70%", "90%", "130" },
+                { "Фильм 1", "50%", "50%", "70%", "120" },
+                { "Фильм 2", "40%", "60%", "80%", "150" },
+                { "Фильм 3", "30%", "70%", "90%", "130" },
+                { "Фильм 1", "50%", "50%", "70%", "120" },
+                { "Фильм 2", "40%", "60%", "80%", "150" },
+                { "Фильм 3", "30%", "70%", "90%", "130" },
+                { "Фильм 1", "50%", "50%", "70%", "120" },
+                { "Фильм 2", "40%", "60%", "80%", "150" },
+                { "Фильм 3", "30%", "70%", "90%", "130" },
+                { "Фильм 1", "50%", "50%", "70%", "120" },
+                { "Фильм 2", "40%", "60%", "80%", "150" },
+                { "Фильм 3", "30%", "70%", "90%", "130" },
+                { "Фильм 1", "50%", "50%", "70%", "120" },
+                { "Фильм 2", "40%", "60%", "80%", "150" },
+                { "Фильм 3", "30%", "70%", "90%", "130" },
+
+            };
+            AddDataToDataTable(dt3, data3);
+
+            string[] columnNames4 = { "Фильм", "Время начала сеанса", "Длительность",
+                "Цена", "Куплено билетов", "Осталось билетов"};
+            dt4 = CreateDataTable(columnNames4);
+            string[,] data4 = {
+                { "Фильм 1", "14:00", "2:50", "300", "120", "50"},
+                { "Фильм 1", "14:00", "2:50", "300", "120", "50"},
+                { "Фильм 1", "14:00", "2:50", "300", "120", "50"},
+                { "Фильм 1", "14:00", "2:50", "300", "120", "50"},
+                { "Фильм 1", "14:00", "2:50", "300", "120", "50"},
+                { "Фильм 1", "14:00", "2:50", "300", "120", "50"},
+                { "Фильм 1", "14:00", "2:50", "300", "120", "50"},
+                { "Фильм 1", "14:00", "2:50", "300", "120", "50"},
+            };
+            AddDataToDataTable(dt4, data4);
+            dataGridView2.DataSource = dt4;
+
+
+            dataGridView3.DataSource = dt4;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = dt2;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            dataGridView1.DataSource = dt3;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = dt1;
+        }
+
+        private DataTable CreateDataTable(string[] columnNames)
+        {
+            // создаем DataTable и добавляем столбцы в DataTable
+            DataTable dt = new DataTable();
+
+            foreach (string columnName in columnNames)
+            {
+                dt.Columns.Add(columnName, typeof(string));
+            }
+
+            return dt;
+        }
+
+        private void AddDataToDataTable(DataTable dt, string[,] data)
+        {
+            // добавляем строки в DataTable
+            for (int i = 0; i < data.GetLength(0); i++)
+            {
+                DataRow row = dt.NewRow();
+                for (int j = 0; j < data.GetLength(1); j++)
+                {
+                    row[j] = data[i, j];
+                }
+                dt.Rows.Add(row);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // обновляем данные в первой таблице
+            AddDataToDataTable(dt1, data1);
+
+            // обновляем данные во второй таблице
+            AddDataToDataTable(dt2, data2);
+
+            // обновляем данные в третьей таблице        
+            AddDataToDataTable(dt3, data3);
+
+            dataGridView1.DataSource = dt1;
+        }
+
+        //public class Movie
+        //{
+        //    public string Title { get; set; } // Название фильма
+        //    public int Duration { get; set; } // Длительность фильма в минутах
+        //    public int Price { get; set; } // Цена билета на фильм
+
+        //    public Movie(string title, int duration, int price)
+        //    {
+        //        Title = title;
+        //        Duration = duration;
+        //        Price = price;
+        //    }
+        //}
+
+        //public class MovieSchedule
+        //{
+        //    public Movie Movie { get; set; } // Фильм, который будет показываться на сеансе
+        //    public string StartTime { get; set; } // Время начала сеанса
+
+        //    public MovieSchedule(Movie movie, DateTime startTime, DateTime endTime)
+        //    {
+        //        Movie = movie;
+        //        StartTime = startTime;
+        //        EndTime = endTime;
+        //    }
+        //}
+
+        //public List<MovieSchedule> GenerateShowtimes(List<Movie> movies, TimeSpan startTime, TimeSpan endTime, TimeSpan interval)
+        //{
+        //    var schedules = new List<MovieSchedule>();
+
+        //    foreach (var movie in movies)
+        //    {
+        //        // Создание расписания для каждого дня недели
+        //        for (int i = 0; i < 7; i++)
+        //        {
+        //            var currentDate = DateTime.Today.AddDays(i);
+        //            var currentTime = currentDate + startTime;
+
+        //            // Создание расписания сеансов для каждого фильма на конкретный день
+        //            while (currentTime <= currentDate + endTime)
+        //            {
+        //                schedules.Add(new MovieSchedule(movie, currentTime, currentTime + movie.Duration));
+        //                currentTime += interval;
+        //            }
+        //        }
+        //    }
+
+        //    return schedules;
+        //}
+
+
+        //public static Dictionary<DayOfWeek, List<MovieSchedule>> GenerateSchedule(List<Movie> movies)
+        //{
+        //    var scheduleByDayOfWeek = new Dictionary<DayOfWeek, List<MovieSchedule>>();
+
+        //    // генерируем расписание для каждого дня недели
+        //    foreach (DayOfWeek dayOfWeek in Enum.GetValues(typeof(DayOfWeek)))
+        //    {
+        //        var movieSchedules = new List<MovieSchedule>();
+
+        //        // генерируем расписание для каждого фильма
+        //        foreach (var movie in movies)
+        //        {
+        //            var showtimes = GenerateShowtimes(dayOfWeek, movie.Duration);
+
+        //            if (showtimes.Count > 0)
+        //            {
+        //                var movieSchedule = new MovieSchedule(movie, showtimes);
+        //                movieSchedules.Add(movieSchedule);
+        //            }
+        //        }
+
+        //        scheduleByDayOfWeek[dayOfWeek] = movieSchedules;
+        //    }
+
+        //    return scheduleByDayOfWeek;
+        //}
+
+        //// метод для заполнения ComboBox фильмами
+        //private void PopulateMovieComboBox()
+        //{
+        //    // очищаем ComboBox и добавляем все фильмы из movies в него
+        //    comboBoxMovies.Items.Clear();
+        //    foreach (var movie in movies)
+        //    {
+        //        comboBoxMovies.Items.Add(movie);
+        //    }
+        //}
+
+        //// метод для обновления расписания в DataGridView
+        //private void UpdateSchedule()
+        //{
+        //    // получаем выбранный день недели из ComboBox
+        //    DayOfWeek dayOfWeek = (DayOfWeek)comboBoxDayOfWeek.SelectedItem;
+
+        //    // получаем выбранный фильм из ComboBox
+        //    Movie selectedMovie = (Movie)comboBoxMovies.SelectedItem;
+
+        //    // генерируем расписание сеансов для выбранного дня недели
+        //    var schedule = GenerateSchedule(movies)[dayOfWeek];
+
+        //    // если выбран фильм, фильтруем расписание по фильму
+        //    if (selectedMovie != null)
+        //    {
+        //        schedule = schedule.Where(ms => ms.Movie == selectedMovie).ToList();
+        //    }
+
+        //    // заполняем DataGridView расписанием сеансов
+        //    dataGridViewSchedule.Rows.Clear();
+        //    foreach (var movieSchedule in schedule)
+        //    {
+        //        var row = new DataGridViewRow();
+        //        row.CreateCells(dataGridViewSchedule);
+        //        row.Cells[0].Value = movieSchedule.Movie.Title;
+        //        row.Cells[1].Value = string.Join(", ", movieSchedule.Showtimes.Select(st => st.ToString("hh:mm tt")));
+        //        dataGridViewSchedule.Rows.Add(row);
+        //    }
+        //}
+
     }
 }
