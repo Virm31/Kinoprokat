08ef4130408d2eddd029b4034519ed934eefa80d
Virm31 <max.zaxqazaq@gmail.com>
2023-03-01 16:36:04 +0500

    Теперь точно полный

---
+++ DataBase/HallAction.cs
@@ -0,0 +1,115 @@
+﻿using Kinoprokat.Classes;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace Kinoprokat.DataBase
+{
+    class HallAction
+    {
+        private readonly string _connectionString;
+
+        public HallAction(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void AddHall(string name, int capacity, string screen_type)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                var command = new SqlCommand("INSERT INTO halls (name, capacity, screen_type) VALUES (@Name, @Capacity, @ScreenType)", connection);
+                command.Parameters.AddWithValue("@Name", name);
+                command.Parameters.AddWithValue("@Capacity", capacity);
+                command.Parameters.AddWithValue("@ScreenType", screen_type);
+
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void UpdateHall(int id, string name, int capacity, string screen_type)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                var command = new SqlCommand("UPDATE halls SET name = @Name, capacity = @Capacity, screen_type = @ScreenType WHERE id = @Id", connection);
+                command.Parameters.AddWithValue("@Id", id);
+                command.Parameters.AddWithValue("@Name", name);
+                command.Parameters.AddWithValue("@Capacity", capacity);
+                command.Parameters.AddWithValue("@ScreenType", screen_type);
+
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void DeleteHall(int id)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                var command = new SqlCommand("DELETE FROM halls WHERE id = @Id", connection);
+                command.Parameters.AddWithValue("@Id", id);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public List<Hall> GetHalls()
+        {
+            var halls = new List<Hall>();
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                var command = new SqlCommand("SELECT * FROM halls", connection);
+
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    var hall = new Hall()
+                    {
+                        Id = reader.GetInt32(0),
+                        Name= reader.GetString(1),
+                        Capacity= reader.GetInt32(2),
+                        ScreenType= reader.GetString(3)
+                    };
+
+                    halls.Add(hall);
+                }
+            }
+
+            return halls;
+        }
+
+        public Hall GetHallById(int id)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                var command = new SqlCommand("SELECT * FROM halls WHERE id = @Id", connection);
+                command.Parameters.AddWithValue("@Id", id);
+
+                var reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    return new Hall()
+                    {
+                        Id = reader.GetInt32(0),
+                        Name = reader.GetString(1),
+                        Capacity = reader.GetInt32(2),
+                        ScreenType = reader.GetString(3)
+                    };
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
---
+++ DataBase/SeatActions.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using Kinoprokat.Classes;
+
+namespace Kinoprokat.DataBase
+{
+    class SeatActions
+    {
+        private readonly string _connectionString;
+
+        public SeatActions(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void AddSeat(int hall_id , int row, int number)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                var command = new SqlCommand("INSERT INTO seats (hall_id, row, number) VALUES (@HallId, @Row, @Number)", connection);
+                command.Parameters.AddWithValue("@HallId", hall_id);
+                command.Parameters.AddWithValue("@Row", row);
+                command.Parameters.AddWithValue("@Number", number);
+
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void UpdateSeat(int id, int hall_id, int row, int number)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                var command = new SqlCommand("UPDATE seats SET hall_id= @HallId, row= @Row, number= @Number WHERE id = @Id", connection);
+                command.Parameters.AddWithValue("@Id", id);
+                command.Parameters.AddWithValue("@HallId", hall_id);
+                command.Parameters.AddWithValue("@Row", row);
+                command.Parameters.AddWithValue("@Number", number);
+
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void DeleteSeat(int id)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                var command = new SqlCommand("DELETE FROM seats WHERE id = @Id", connection);
+                command.Parameters.AddWithValue("@Id", id);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public List<Seat> GetSeats()
+        {
+            var seats = new List<Seat>();
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                var command = new SqlCommand("SELECT * FROM seats", connection);
+
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    var seat = new Seat()
+                    {
+                        Id = reader.GetInt32(0),
+                        HallId= reader.GetInt32(1),
+                        Row= reader.GetInt32(2),
+                        Number=reader.GetInt32(3)
+                    };
+
+                    seats.Add(seat);
+                }
+            }
+
+            return seats;
+        }
+
+        public Seat GetSeatById(int id)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                var command = new SqlCommand("SELECT * FROM seats WHERE id = @Id", connection);
+                command.Parameters.AddWithValue("@Id", id);
+
+                var reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    return new Seat()
+                    {
+                        Id = reader.GetInt32(0),
+                        HallId = reader.GetInt32(1),
+                        Row = reader.GetInt32(2),
+                        Number = reader.GetInt32(3)
+                    };
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
