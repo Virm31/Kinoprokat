36e625ec66c323806af7cfa0c937833e74aae1b8
Salikh Valitov <salikhcomhd@gmail.com>
2023-03-07 20:19:16 +0500

    Новый

--- Form4.cs
+++ Form4.cs
@@ -83,7 +83,7 @@ namespace Kinoprokat
 
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
-           
+           //Хуй залупа
         }
 
         private void SessionsEditor_Load(object sender, EventArgs e)
